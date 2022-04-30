0f8d5a19b3d10aec3660abba82209675647c5e17
dpkn1989 <dpkn1989@gmail.com>
2022-04-30 10:04:19 +0530

    Created a simple factorial code in another source file

---
+++ my_math.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void print_factorial(int& a)
+{
+    int k = 1;
+	cout << "Address of " << a << " is: " << &a << endl;
+
+    if(a == 0)
+	{
+		cout << "Factorial of zero is " << k << endl;
+	}
+	else if(a < 0)
+	{
+		cout << "Factorial of a negative number is invalid" << endl;
+	}
+	else{
+        for(int j = 1; j <= a; j++)
+	    {  
+		    k *= j; 
+	    }
+		cout << "Factorial of " << a << " is: " << k << endl;
+	}
+}
